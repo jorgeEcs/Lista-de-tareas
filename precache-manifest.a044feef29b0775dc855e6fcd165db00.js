d5dd8d1c556d33faaf1c78df877f21ba3656340f
Jorge Callisaya <jcelbarto@gmail.com>
2020-10-30 21:44:05 -0400

    Updates

---
+++ precache-manifest.a044feef29b0775dc855e6fcd165db00.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2a5029c12c8737b014091a326063715",
+    "url": "/Lista-de-tareas/index.html"
+  },
+  {
+    "revision": "5c8238c916f407d1291f",
+    "url": "/Lista-de-tareas/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "e8bb936b6604f00e1530",
+    "url": "/Lista-de-tareas/static/css/main.93b97114.chunk.css"
+  },
+  {
+    "revision": "5c8238c916f407d1291f",
+    "url": "/Lista-de-tareas/static/js/2.f9afda74.chunk.js"
+  },
+  {
+    "revision": "5c5a0857bb8747e9ae7db7917ecbbf79",
+    "url": "/Lista-de-tareas/static/js/2.f9afda74.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8bb936b6604f00e1530",
+    "url": "/Lista-de-tareas/static/js/main.7e469e10.chunk.js"
+  },
+  {
+    "revision": "eb6fbbb53a04a132eeb2",
+    "url": "/Lista-de-tareas/static/js/runtime-main.206e1195.js"
+  }
+]);
